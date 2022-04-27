463dd1f1ef03caa95283c36e952adcd556824d67
ImaginaryElf <imaginary.elf.2718@gmail.com>
2022-04-27 20:44:37 +0100

    add initial files

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+api = Api(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+class HealthCheckApi(Resource):
+    @cross_origin()
+    def get(self):
+        return {'status': True}
+
+
+api.add_resource(HealthCheckApi, '/health_check')
+
+if __name__ == '__main__':
+    app.run(port='5002')
